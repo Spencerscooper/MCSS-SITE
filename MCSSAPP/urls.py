f93a785f74025e254e6ca907a816bc8f93d6f092
Spencerscooper <spencercooper1991@gmail.com>
2023-11-10 03:52:19 +0000

    editing all files in the project

--- MCSSAPP/urls.py
+++ MCSSAPP/urls.py
@@ -3,11 +3,16 @@ from . import views
 
 urlpatterns =[
     path('', views.index, name='index'),
-    path('aboutmcss', views.aboutmcss, name='aboutmcss'),
-    path('mcssadministration', views.mcssadministration, name='mcssadministration'),
+    path('about', views.about, name='about'),
+    path('administration', views.administration, name='administration'),
     path('news', views.news, name='news'),
-    path('tubmanhigh', views.tubmanhigh, name='tubmanhigh'),#for tubman high schools
-    path('tubman_admin', views.tubman_admin, name='tubman_admin'),
-    path('Schedule', views.Schedule, name='Schedule')
+    path('<int:pk>/', views.newsdetails, name='newsdetails'),
+    path('seniormanagement/<int:image_id>', views.seniormanagement)
+    # path('aboutmcss', views.aboutmcss, name='aboutmcss'),
+    # path('mcssadministration', views.mcssadministration, name='mcssadministration'),
+    # path('news', views.news, name='news'),
+    # path('tubmanhigh', views.tubmanhigh, name='tubmanhigh'),#for tubman high schools
+    # path('tubman_admin', views.tubman_admin, name='tubman_admin'),
+    # path('Schedule', views.Schedule, name='Schedule')
 
 ]
